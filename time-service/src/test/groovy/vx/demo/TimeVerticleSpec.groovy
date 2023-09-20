2bee127900941aafbd1490bdb293de97ff4e1629
TEST\smirnov <konstantin.smirnov@hausbank.de>
2023-09-20 16:47:18 +0200

    no message

--- time-service/src/test/groovy/vx/demo/TimeVerticleSpec.groovy
+++ time-service/src/test/groovy/vx/demo/TimeVerticleSpec.groovy
@@ -1,7 +1,10 @@
 package vx.demo
 
+import groovy.json.JsonSlurper
 import io.vertx.core.Future
+import io.vertx.core.http.HttpMethod
 import io.vertx.ext.healthchecks.Status
+import vx.demo.domain.LogEvent
 import vx.demo.gorm.StatisticsService
 import vx.demo.test.VertxSpecification
 
@@ -10,10 +13,10 @@ class TimeVerticleSpec extends VertxSpecification {
   def 'send valid timeZone over event-bus and get formatted time string back'() {
     given:
     String timeZone = 'Europe/Berlin'
-    String res
-    boolean success
     
     when:
+    String res
+    boolean success
     vertx.eventBus().request( 'time', timeZone ){
       success = it.succeeded()
       res = it.result()?.body()
@@ -51,15 +54,43 @@ class TimeVerticleSpec extends VertxSpecification {
     def futs = []
     4.times{ futs << vertx.eventBus().request( 'time', 'GMT' ) }
     6.times{ futs << vertx.eventBus().request( 'time', 'INVALID' ) }
-    
-    then: 'wait for completion and check that stats numbers grew'
+
+    and:
     Status result
     Future.join futs onComplete{ result = ss.collectStats() }
-    
+        
+    then: 'wait for completion and check that stats numbers grew'
     await{
       result.data.map.ok == initial.data.map.ok + 4
       result.data.map.err == initial.data.map.err + 6
     }
   }
   
+  def 'health monitoring endpoint should provide statistics'() {
+    setup: 'clear stats'
+    LogEvent.withTransaction{
+      LogEvent.executeUpdate 'delete from LogEvent'
+    }
+    def ss = new StatisticsService( 'time' )
+    
+    when: 'send 3 good + 1 bad requests over EB'
+    def futs = []
+    3.times{ futs << vertx.eventBus().request( 'time', 'GMT' ) }
+    1.times{ futs << vertx.eventBus().request( 'time', 'INVALID' ) }
+    
+    and:
+    Map status
+    Future.join futs onComplete{ 
+      status = new JsonSlurper().parseText new URL( 'http://localhost:8092/health' ).text
+    }
+    
+    then: 'wait for completion and check status'
+    await{
+      status.status == 'UP'
+      status.checks[ 0 ].id == 'TimeVerticle'
+      status.checks[ 0 ].data.err == 1
+      status.checks[ 0 ].data.ok == 3
+    }
+  }
+
 }
